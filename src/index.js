482f2ce04682e9ce9349cb0c1381cb11c11cf7d1
shlokaskshetty <122727116+shlokaskshetty@users.noreply.github.com>
2023-11-21 13:25:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root')).render(
+ ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <App />
   </Router>
